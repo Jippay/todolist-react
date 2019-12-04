a3c7878bbc6c442943be91d8b0ea557def924cdc
Jean-Philippe GRASSE <grassejp@gmail.com>
2019-12-04 12:03:39 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from './Todo'
+
+function App () {
+  return (
+    <div className='container'>
+      <Todo />
+
+    </div>
+  )
+}
+
+export default App
