78d75d59870f8f6a167735a399af194d46621294
Jean-Philippe GRASSE <grassejp@gmail.com>
2019-12-04 12:10:00 +0100

    Updates

---
+++ precache-manifest.ebe621d483714148a5a8df8394ec8620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34135bb754d4ac9ee2af2c06595474ca",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "350f4a14534b5388403e",
+    "url": "/todolist-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "350f4a14534b5388403e",
+    "url": "/todolist-react/static/js/2.c1e98df6.chunk.js"
+  },
+  {
+    "revision": "126bd5507a86e15d59a9",
+    "url": "/todolist-react/static/js/main.ba625f6b.chunk.js"
+  },
+  {
+    "revision": "34a495385f77aa498414",
+    "url": "/todolist-react/static/js/runtime-main.2fb88b90.js"
+  }
+]);
